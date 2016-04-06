911c0ab8db12d06e33074ae867b66e106577da81
Chris Boone <cboone1337@gmail.com>
2016-04-06 16:25:03 -0500

    user can add contacts

---
+++ app/controllers/index.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  addPerson() {
+    const contact = {
+      first: this.first,
+      last: this.last,
+      address: this.address,
+      phone: this.phone,
+    };
+    fetch(`https://tiny-tn.herokuapp.com/collections/cb-people`, {
+      method: `POST`,
+      headers: {
+        Accept: `application/json`,
+        'Content-type': `application/json`,
+      },
+      body: JSON.stringify(contact)
+    }).then((res) => res.json())
+    .then((person) => {
+      this.clearForm();
+      this.addNewContact(person);
+    });
+  },
+
+  clearForm() {
+    this.set(`first`, ``);
+    this.set(`last`, ``);
+    this.set(`address`, ``);
+    this.set(`phone`, ``);
+  },
+
+  addNewContact() {
+
+  }
+});
